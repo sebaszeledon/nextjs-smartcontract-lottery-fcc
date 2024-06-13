346045606568f972ca3272c0a9ed24c702c7608f
sebaszeledon <sebas.zeledon@gmail.com>
2024-06-13 10:59:52 -0600

    update

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,15 +1,35 @@
 import { useWeb3Contract } from "react-moralis"
-import { abi, contractAddress } from "../constants";
+import { abi, contractAddresses } from "../constants";
+import { useMoralis } from "react-moralis";
+import { useEffect } from "react";
 
 export default function LotteryEntrance() {
 
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
+    const chainId = parseInt(chainIdHex);
+    const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null;
+
     // const { runContractFunction: enterRaffle } = useWeb3Contract({
-    //     abi: //,
-    //     contractAddress: //,
-    //     functionName: //,
-    //     params {},
+    //     abi: abi,
+    //     contractAddress: raffleAddress,
+    //     functionName: "enterRaffle",
+    //     params: {},
     //     msgValue: //
     // });
 
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    });
+
+    useEffect(() => {
+        if (isWeb3Enabled){
+            //Try to read the raffle entrance fee
+            
+        }
+    }, []);
+
     return <div> Hi from lottery entrance!</div>
-}//17:38:38
+}
