f76ab01a2debd71b291027e0048c6b4d5f81722c
sebaszeledon <sebas.zeledon@gmail.com>
2024-06-12 21:49:01 -0600

    update

---
+++ components/LotteryEntrance.js
@@ -0,0 +1,15 @@
+import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddress } from "../constants";
+
+export default function LotteryEntrance() {
+
+    // const { runContractFunction: enterRaffle } = useWeb3Contract({
+    //     abi: //,
+    //     contractAddress: //,
+    //     functionName: //,
+    //     params {},
+    //     msgValue: //
+    // });
+
+    return <div> Hi from lottery entrance!</div>
+}//17:38:38
